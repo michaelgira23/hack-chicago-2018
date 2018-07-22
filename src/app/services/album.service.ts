41354225f0a449ba982b76eba7587626adc17cb5
AsianPsychoBoy <ziyang_99@outlook.com>
2018-07-21 22:00:25 -0500

    fix

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -1,22 +1,35 @@
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Album } from '../models/album.model';
 import { Injectable } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
 import * as firebase from 'firebase';
+import { LocationService } from '../services/location.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private locationService: LocationService) { }
 
   getAllAlbums() {
     return this.db.list<Album>('albums').valueChanges();
   }
 
-  createAlbum(album: CreateAlbumOptions) {
-    album.created = firebase.database.ServerValue.TIMESTAMP;
-    return this.db.list<Album>('albums').push(album);
+  createAlbum(options: CreateAlbumOptions) {
+    return this.locationService.getLocation().pipe(
+        switchMap(location => {
+            const album: Album = {
+                name: options.name,
+                discoverable: options.discoverable,
+                created: firebase.database.ServerValue.TIMESTAMP as any,
+                images: {},
+                passcode: options.passcode,
+                location
+            };
+            return this.db.list<Album>('albums').push(album);
+        })
+    );
   }
 
 }
@@ -24,4 +37,5 @@ export class AlbumService {
 export interface CreateAlbumOptions {
   name: string;
   discoverable: boolean;
+  passcode?: string;
 }
